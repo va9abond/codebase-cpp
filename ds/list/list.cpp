bdd3780db90fcb91a55adac0b7e90ef4988bf581
sirazetdinov r <avesirazetdinov@gmail.com>
2023-03-12 14:52:24 +0300

    linked_list: complete Accessors & Mutators; add Modifiers

--- ds/list/list.cpp
+++ ds/list/list.cpp
@@ -1,6 +1,7 @@
 #include "iterator.cpp"
 // #include <exception>
 // #include <limits.h>
+#include <limits.h>
 #include <limits>
 #include <memory>
 #include <stdlib.h>
@@ -29,6 +30,7 @@ public:
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 
+    /* Ctors, Dtor & Assignment Operators */
 
     linked_list() noexcept :
         _size(0),
@@ -36,24 +38,23 @@ public:
     {}
 
     // TODO: need check
-    explicit linked_list (size_t size, const_reference data = Ty_() ) :
+    explicit linked_list (size_t size, const_reference value = Ty_() ) :
     // **be careful**
     // type Ty_ must have default ctor
-        _size(size),
+        // _size(size),
         _head(nullptr)
     {
-        try 
-        {
-            if (size <= 0 || size > _MAX_CAPACITY) {
+        try {
+            if (size <= 0 || size > UINT_MAX) {
                 _size = 0;
                 throw _ml exception("Invalid size");
             }
 
             _ALLOCATE_HEAD();
-            _head->_prev = _head->_next = new Node_(data, _head, _head);
+            _head->_prev = _head->_next = new Node_(value, _head, _head);
             
             while (size --> 0) { 
-                _EMPLACE_BACK_NOCHECK( std::move( Ty_(data) ) ); 
+                _EMPLACE_BACK( std::move( Ty_(value) ) ); 
                 // there I use std::move instead of std::forward, because
                 // Ty_(data) return rvalue of type Ty_
             }
@@ -70,29 +71,38 @@ protected:
     }
 
     // TODO: need check
-    Nodeptr_ _EMPLACE_NOCHECK (Nodeptr_ where, Ty_&& data) noexcept { // new element will be emplaced at where
-        
-        Nodeptr_ new_node = new Node_(std::forward<Ty_>(data), where->_prev, where);
-        
-        where->_prev->_next = new_node;
-        where->_prev = new_node;
+    Nodeptr_ _EMPLACE (Nodeptr_ where, Ty_&& data) noexcept { // create element at where
+        try {
+            
+            if (_size == max_size()) {
+                throw _ml exception("list too long");
+            }
 
-        return new_node;
+            Nodeptr_ new_node = new Node_(std::forward<Ty_>(data), where->_prev, where);
+            where->_prev->_next = new_node;
+            where->_prev = new_node;
+
+            _size++;
+            return new_node;
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
     }
 
-    Nodeptr_ _EMPLACE_BACK_NOCHECK (Ty_&& data) noexcept {
-        return _EMPLACE_NOCHECK(_head, std::forward<Ty_>(data));
+    Nodeptr_ _EMPLACE_BACK (Ty_&& data) noexcept {
+        return _EMPLACE(_head, std::forward<Ty_>(data));
     }
 
-    Nodeptr_ _EMPLACE_FRONT_NOCHECK (Ty_&& data) noexcept {
-        return _EMPLACE_NOCHECK(_head->_next, std::forward<Ty_>(data));
+    Nodeptr_ _EMPLACE_FRONT (Ty_&& data) noexcept {
+        return _EMPLACE(_head->_next, std::forward<Ty_>(data));
     }
 
 
 public:
     // TODO: need check
     linked_list (const linked_list& right) noexcept : // deep copy
-        _size(right._size),
+        // _size(right._size),
         _head(nullptr)
     {
         if (_size != 0)
@@ -102,7 +112,7 @@ public:
             auto right_nodeptr = right._head->_next;
             while (right_nodeptr != nullptr) 
             {
-                _EMPLACE_BACK_NOCHECK( Ty_(right_nodeptr->_data) );
+                _EMPLACE_BACK( Ty_(right_nodeptr->_data) );
                 right_nodeptr = right_nodeptr->_next;
             }
 
@@ -126,7 +136,7 @@ public:
                 auto right_nodeptr = right._head->_next;
                 while (right_nodeptr != nullptr) 
                 {
-                    _EMPLACE_BACK_NOCHECK( Ty_(right_nodeptr->_data) );
+                    _EMPLACE_BACK( Ty_(right_nodeptr->_data) );
                     right_nodeptr = right_nodeptr->_next;
                 }
             }
@@ -142,10 +152,10 @@ public:
     {
         _ALLOCATE_HEAD();
         _head->_prev = right._head->_prev;
-        _head->_next = right._head->_next;
+        _head->_next = right._head->_next; // TODO: SWAP HEADS
 
         right._size  = 0;
-        right._head = nullptr; // ::operator delete(right._head); MEMORY LEAK
+        right._head = nullptr; //TODO: operator delete(right._head); MEMORY LEAK
     }
 
     // TODO: need check
@@ -170,7 +180,7 @@ public:
         size_t size
     >
     linked_list (T_ (&&init_list)[size]) noexcept :
-        _size(size),
+        // _size(size),
         _head(nullptr)
     {
         if (size > 0)
@@ -179,14 +189,61 @@ public:
             _head->_prev = _head->_next = new Node_(std::move(init_list[0]), _head, _head);
             
             for (size_t i = 1; i < size; i++) {
-                _EMPLACE_BACK_NOCHECK(std::move(init_list[i]));
+                _EMPLACE_BACK(std::move(init_list[i]));
             }
         }
     }
 
+
+    /* Iterators */
+
+
+    iterator begin() noexcept {
+
+    }
+
+    const_iterator begin() const noexcept {
+
+    }
+
+    iterator end() noexcept {
+
+    }
+
+    const_iterator end() const noexcept {
+
+    }
+
+protected:
+
+    iterator _MAKE_ITER (Nodeptr_ Where_) const noexcept {
+
+    }
+
+    const_iterator _MAKE_CONST_ITER (Nodeptr_ Where_) const noexcept {
+
+    }
+
+public:
+
+    // TODO: reverse iterators
+
+    const_iterator cbegin() const noexcept {
+
+    }
+
+    const_iterator cend() const noexcept {
+
+    }
+
+    // TODO: const reverse iterators
+
+
+    /* Accessors & Mutators */
+
+
     Ty_& operator[] (size_t index) const {
-        try
-        {
+        try {
             if (index < 0 || index >= _size) {
                 throw _ml exception("Invalid index");
             }
@@ -203,25 +260,198 @@ public:
         }
     }
 
+    size_t size() const noexcept {
+        return _size;
+    }
+
+    size_t max_size() const noexcept {
+        return UINT_MAX;
+    }
+
+    bool empty() const noexcept {
+        return _size == 0;
+    }
+
+    reference front() { 
+        try {
+            if ( _size == 0 ) {
+                throw _ml exception("front() called on empty list");
+            }
+
+            return _head->_next->_data;     
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
+    }  
+        
+    const_reference front() const {
+        try {
+            if ( _size == 0 ) {
+                throw _ml exception("front() called on empty list");
+            }
+
+            return _head->_next->_data;     
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
+    }
 
 
+    reference back() { 
+        try { 
+            if ( _size == 0 ) {
+                throw _ml exception("back() called on empty list");
+            }
+
+            return _head->_prev->_data;     
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
+    }
+        
+    const_reference back() const { 
+        try {
+            if ( _size == 0 ) {
+                throw _ml exception("back() called on empty list");
+            }
+
+            return _head->_prev->_data;     
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
+    }
 
 
+    /* Modifiers */
 
 
-    void clear() noexcept {}
+    void push_front (value_type&& data) {
+        _EMPLACE(_head->_next, std::move(data));
+        // _EMPLACE_FRONT(std::move(data));
+    }
 
-private:
+    void push_back (value_type&& data) {
+        _EMPLACE(_head, std::move(data));
+        // _EMPLACE_BACK(std::move(data));
+    }
 
+    void push_front (const_reference data) {
+        _EMPLACE(_head->_next, data);
+    }
 
+    void push_back (const_reference data) { // TODO: vs. _EMPLACE(_head, data);
+        _EMPLACE_BACK(data);
+    }
 
+    void pop_front() {
+        try {
+            if (_size == 0) {
+                throw _ml exception("pop_front() called on empty list");
+            }
+
+            _UNCHECKED_ERASE(_head->_next);
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
+    }
+
+    void pop_back() {
+        try {
+            if (_size == 0) {
+                throw _ml exception("pop_back() called on empty list");
+            }
+
+            _UNCHECKED_ERASE(_head->_prev);
+        }
+        catch (_ml exception length_error) {
+            std::cerr << length_error.what();
+        }
+    }
+
+    iterator emplace (const const_iterator where, value_type&& data) { // insert element at where
+
+    }
+
+    reference emplace_front (value_type&& data) { // insert element at beginning
+
+    }
+
+    reference emplace_back (value_type&& data) { // insert element at end
+
+    }
+
+    iterator insert (const_iterator where, const_reference data) { // insert data at where
+
+    }
+
+    iterator insert (const_iterator where, int count, const_reference data) { // insert count * data before where
+
+    }
+
+    template <
+        class Iter_
+    >
+    iterator insert (const_iterator where, Iter_ First_, Iter_ Last_) { // insert [First_, Last_) before where
+
+    }
+
+    iterator erase (const_iterator where) noexcept {
+
+    }
+
+protected:
+    
+    Nodeptr_ _UNCHECKED_ERASE (const Nodeptr_ Pnode_) {
+
+    }
+
+public:
+
+    iterator erase (const const_iterator First_, const const_iterator Last_) noexcept {
+
+    }
+
+protected:
+
+    Nodeptr_ _UNCHECKED_ERASE (Nodeptr_ First_, const Nodeptr_ Last_) noexcept {
+
+    }
+
+public:
+
+    auto remove (const_reference data) { // erase each element matching data
+
+    }
+
+    template <
+        class PredTy_
+    >
+    auto remove_if (PredTy_ Pred_) { // erase each element satisfying Pred_
+
+    }
+
+    void clear() noexcept {
+
+    }
+
+    void resize (size_t newSize) {
+
+    }
+
+    void resize (size_t newSize, const_reference data) {
+
+    }
 
 
 
 protected:
     size_t   _size;
     Nodeptr_ _head;
-    static const unsigned int _MAX_CAPACITY = UINT_MAX;
 };
 
 
@@ -229,9 +459,3 @@ int main() {
     linked_list<int, DListNode> list;
 }
 
-// Чего я хочу? Я хочу, чтобы 
-//     list.front()->prev = head;
-//     list.back()->next = head;
-//     head->prev = list.back(); 
-//     list.end() = head;
-//     list.begin() = head->next
