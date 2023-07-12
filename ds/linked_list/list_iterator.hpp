1edfea0b3f48d5cb4dd81e97425d27cfde121ad1
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-07-13 02:25:56 +0300

    _List_unchecked_const_iterator done

--- ds/linked_list/list_iterator.hpp
+++ ds/linked_list/list_iterator.hpp
@@ -5,6 +5,13 @@
 #include "list_node.hpp"
 
 
+_MSL_BEGIN
+// [x] _List_unchecked_const_terator
+// [ ] _List_unchecked_iterator
+// [ ] _List_const_iterator
+// [ ] _List_iterator
+
+
 template <
     class _Mylist,
     class _Base = _Iterator_base
@@ -19,7 +26,57 @@ public:
     using pointer         = typename _Mylist::const_pointer;
     using reference       = const value_type&;
 
-    _Nodeptr _Myptr;
+    _List_unchecked_const_iterator() noexcept : _Myptr() {}
+
+    _List_unchecked_const_iterator (_Nodeptr Parent_node, const _Mylist* Parent_list) noexcept : _Myptr(Parent_node) {
+        _Adopt_me(Parent_list); 
+    }
+
+    reference operator*() const noexcept {
+        return _Myptr->_Myval;
+    } 
+
+    pointer operator->() const noexcept {
+        return this;
+    }
+
+    explicit operator bool() const noexcept { // type conversation iterator to bool
+                                              // _Myptr == nullptr ? true : false;
+        return _Myptr;
+    }
+
+    _List_unchecked_const_iterator& operator++() noexcept {
+        _Myptr = _Myptr->_Next;
+        return *this;
+    }
+
+    _List_unchecked_const_iterator& operator++(int) noexcept {
+        _List_unchecked_const_iterator Temp = *this;
+        _Myptr = _Myptr->_Next;
+        return Temp;
+    }
+
+    _List_unchecked_const_iterator& operator--() noexcept {
+        _Myptr = _Myptr->_Prev;
+        return *this;
+    }
+
+    _List_unchecked_const_iterator& operator--(int) noexcept {
+        _List_unchecked_const_iterator Temp = *this;
+        _Myptr = _Myptr->_Prev;
+        return Temp;
+    }
+
+    bool operator== (const _List_unchecked_const_iterator& Rhs) const noexcept {
+        return _Myptr == Rhs._Myptr;
+    }
+    
+    bool operator!= (const _List_unchecked_const_iterator& Rhs) const noexcept {
+        return _Myptr != Rhs._Myptr;
+    }
+
+
+    _Nodeptr _Myptr; // pointer to node
 };
 
 
@@ -76,4 +133,5 @@ public:
     using _Mybase::_Mybase;
 };
 
+_MSL_END
 #endif // LISTITERATOR_HPP
