6da6c102c1666a250ff11733f835203029e2071d
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-07-13 17:46:11 +0300

    done _List_const_iterator

--- ds/linked_list/list_iterator.hpp
+++ ds/linked_list/list_iterator.hpp
@@ -147,6 +147,80 @@ public:
     using reference       = const value_type&;
 
     using _Mybase::_Mybase;
+
+    reference operator*() const noexcept {
+        // this->_Getcont return const _Container_base*
+        // static_cast convert const pointer to parent class _Container_base
+        // to const pointer to one of the derived classes like _List_val in our
+        // case, so, in other words, there is a conversation from parent type
+        // class pointer to derived type class pointer
+        // (*) nullptr has type std::nullptr_t, which convertible to any
+        // raw pointer type
+        const auto Mycont = static_cast<const _Mylist*>(this->_Getcont());
+        _MSL_VERIFY_f(Mycont, "cannot dereference valut-initialized list iterator");
+        _MSL_VERIFY_f(this->_Myptr != Mycont->_Myhead, "cannot dereference end list iterator");
+    }
+
+    pointer operator->() const noexcept {
+        return this;
+    } 
+
+    explicit operator bool() const noexcept {
+        const auto Mycont = static_cast<const _Mylist*>(this->_Getcont());
+        return (this->_Myptr == Mycont->_Myhead) ? false : this->_Myptr;
+    }
+
+    _List_const_iterator& operator++() noexcept {
+        const auto Mycont = static_cast<const _Mylist*>(this->_Getcont());
+        _MSL_VERIFY_f(Mycont, "cannot increment value_initialized list iterator");
+        _MSL_VERIFY_f(this->_Myptr != Mycont->_Myhead, "cannot increment end list iterator");
+
+        this->_Myptr = this->_Myptr->_Next;
+        return *this;
+    }
+
+    _List_const_iterator& operator++(int) noexcept {
+        _List_const_iterator Temp = *this;
+        (*this)++;
+        return Temp;
+    } 
+
+     _List_const_iterator& operator--() noexcept {
+        const auto New_ptr = this->_Myptr->_Prev;
+        const auto Mycont = static_cast<const _Mylist*>(this->_Getcont());
+        _MSL_VERIFY_f(Mycont, "cannot decrement value_initialized list iterator");
+        _MSL_VERIFY_f(New_ptr != Mycont->_Myhead, "cannot decrement begin list iterator");
+        
+        this->_Myptr = New_ptr;
+        return *this;
+    }
+
+    _List_const_iterator& operator--(int) noexcept {
+        _List_const_iterator Temp = *this;
+        --*this;
+        return Temp;
+    }
+
+    bool operator== (const _List_const_iterator& Rhs) const noexcept {
+        _MSL_VERIFY_f(this->_Getcont() == Rhs._Getcont(), "list iterators incompatible");
+        return this->_Myptr == Rhs._Myptr;
+    }
+    
+    bool operator!= (const _List_const_iterator& Rhs) const noexcept {
+        return !(*this == Rhs);
+    }
+
+    friend void _Verify_range (const _List_const_iterator& First, const _List_const_iterator& Last) noexcept {
+        _MSL_VERIFY_f(First->_Getcont() == Last->_Getcont(), "list iterators in range are from different containers");
+    }
+
+    _List_unchecked_const_iterator<_Mylist> _Unwrapped() const noexcept { // make other unchecked iterator base on this
+        return _List_unchecked_const_iterator<_Mylist>(this->_Myptr, static_cast<const _Mylist*>(this->_Getcont()));
+    }
+
+    // void _Seek_to (const _List_unchecked_const_iterator<_Mylist> It) noexcept {
+    //     this->_Myptr = It._Myptr;
+    // }
 };
 
 
