c9dbd499b1bd85f9e7a02d5e6b0c9967a8c999b6
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-07-15 00:14:15 +0300

    in process list_v2

--- ds/linked_list/linked_list.cpp
+++ ds/linked_list/linked_list.cpp
@@ -51,8 +51,10 @@ public:
     using const_pointer   = typename _Val_types::const_pointer;
     using reference       = value_type&;
     using const_reference = const value_type&;    
+    
+    _List_val() noexcept : _Myhead(), _Mysize(0) {} // NOTE: doesn't construct _Container_base implicitly
 
-    _List_val() noexcept : _Myhead(), _Mysize(0) {}
+    _List_val (const _List_val& Rhs) noexcept : _Myhead(), _Mysize(Rhs._Mysize) {} // NOTE: mb it't unneccessary
 
     void _Oprhan_iterators (_Nodeptr Ptr) noexcept;
 
@@ -158,6 +160,7 @@ private:
                             _Nodeptr
                       >;
     using _List_scary_val = _List_val<_Val_types>; // _Scary_val in STL
+    // using list = list_v2;
 
 public:
     static_assert(std::is_same_v<_Ty, typename _Alloc::value_type>,
@@ -184,7 +187,132 @@ public:
 
     // _Compressed_pair<_Alnode, _List_scary_val> _Mypair;
     _List_scary_val _Mycont;
+   
+    list_v2() noexcept : _Mycont() {
+        _Alloc_head_and_proxy();
+    } 
 
+private:
+    void _Construct_n (size_type Count);
+
+public:
+    explicit list_v2 (size_type Count) : _Mycont() {
+        _Construct_n(Count);
+    }
+
+private:
+    void _Construct_n (size_type Count, const _Ty& Val);
+
+public:
+    list_v2 (size_type Count, const _Ty& Val) : _Mycont() {
+        _Construct_n(Count, Val);
+    }
+
+private:
+    template <class _Iter_t>
+    void _Construct_range_unchecked_my (_Iter_t First, _Iter_t Last);
+
+public:
+    list_v2 (const list_v2& Rhs) : _Mycont(Rhs._Mycont) {
+        // _Ald_verify_range();
+        _Construct_range_unchecked_my(Rhs._Unchecked_begin(), Rhs._Unchecked_end());
+    }
+
+    list_v2 (list_v2&& Rhs); // TODO
+
+    list_v2& operator= (list_v2&& Rhs) noexcept;
+
+private:
+    void _Swap_val (list_v2& Rhs) noexcept; // swap with Rhs, same allocator
+
+public:
+    void push_front (_Ty&& Val); // insert element at beginning
+
+    void push_back (_Ty&& Val); // insert element at end
+
+    iterator insert (const_iterator Where, _Ty&& Val); // Val at Where
+
+    decltype(auto) emplace_front (_Ty&& Val); // insert element at beggining
+
+    decltype(auto) emplace_back (_Ty&& Val); // insert element at end
+
+    iterator emplace (const const_iterator Where, _Ty&& Val); // insert element at Where
+
+    _Nodeptr _Emplace (const _Nodeptr Where, _Ty&& Val); // insert element at Where
+
+    // list_v2 (initializer_list<_Ty> Ilist) : _Mycont() {
+    //     _Construct_range_unchecked_my(Ilist.begin(), Ilist.end());
+    // }
+
+    ~list_v2() noexcept;
+    
+    list_v2& operator= (const list_v2& Rhs);
+
+    iterator begin() noexcept;
+
+    const_iterator begin() const noexcept;
+
+    iterator end() noexcept;
+
+    const_iterator end() const noexcept;
+
+    _Unchecked_iterator _Unchecked_begin() noexcept;
+
+    _Unchecked_const_iterator _Unchecked_begin() const noexcept;
+
+    _Unchecked_iterator _Unchecked_end() noexcept;
+
+    _Unchecked_const_iterator _Unchecked_end() const noexcept;
+
+    iterator _Make_iter(_Nodeptr Where) const noexcept;
+
+    const_iterator _Make_const_iterator (_Nodeptr Where) const noexcept;
+
+    reverse_iterator rbegin() noexcept {
+        return reverse_iterator(end());
+    }
+
+    const_reverse_iterator rbegin() const noexcept {
+        return const_reverse_iterator(end());
+    }
+
+    reverse_iterator rend() noexcept {
+        return reverse_iterator(begin());
+    }
+    
+    const_reverse_iterator rend() const noexcept {
+        return const_reverse_iterator(begin());
+    }
+
+    const_iterator cbegin() const noexcept {
+        return begin();
+    }
+
+    const_iterator cend() const noexcept {
+        return end();
+    }
+
+    const_reverse_iterator crbegin() const noexcept {
+        return rbegin();
+    }
+
+    const_reverse_iterator crend() const noexcept {
+        return rend();
+    }
+
+
+private:
+    void _Alloc_head_and_proxy() { // NOTE: _Mycont doesn't exist yet in ctor list_v2()
+        // _Container_proxy* Newproxy = new _Container_proxy(&_Mycont); // TODO: delete
+        _Mycont._Container_base::_Alloc_proxy();
+        _Nodeptr Newhead = static_cast<_Nodeptr>(::operator new(sizeof(_Node))); // TODO: operator delete
+        _Mycont._Myhead = Newhead;
+        Newhead->_Next = Newhead; Newhead->_Prev = Newhead;
+    }
+    
+    void _Orphan_all() noexcept {
+        _Mycont._Orphan_all();
+    } 
 };
     
 _MSL_END
