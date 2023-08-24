06dc4fae436940600c9d6303e7fdd2c2a1cd1071
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-08-24 23:32:02 +0300

    refactor: implement verts as pointers in Graph_base_ struct
    wip: issue #2

--- algo/graphs/acadimic_practice/graph.hpp
+++ algo/graphs/acadimic_practice/graph.hpp
@@ -1,10 +1,13 @@
+/* [[WARNING]]: */
+
+
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
 
 #include "vector_utils.hpp"
 #include <cassert>
-#include <limits.h>
+#include <limits>
 
 // [TODO]: split weight function and graph
 //         (it makes possible to passing one graph with different weight functions)
@@ -13,44 +16,56 @@
 
 
 struct Graph_base_ {
-    //                  Traits
-    // +------------------------------------------+
-    // |          pair.first ~ source;            |
-    // |          pair.second ~ target;           |
-    /* | */   using edge = std::pair<int,int>; // |
-    /* | */   using size_type = unsigned;      // |
-    /* | */   using vert      = int;           // |
-    // +------------------------------------------+
+    //                      Traits
+    // +---------------------------------------------------------+
+    // |             pair.first ~ source;                        |
+    // |             pair.second ~ target;                       |
+    /* | */   using vert      = int;                          // |
+    /* | */   using edge      = std::pair<vert*,vert*>;       // |
+    /* | */   using size_type = std::set<vert*>::size_type;   // |
+    // +---------------------------------------------------------+
+    // [WARNING]: Despite the fact that size_type is usigned long
+    //            for std::set<vert*>, we allow to create just
+    //            INT_MAX vertice!
 
-    Graph_base_(size_type vertsCount = 0) : m_Verts() {
-        Construct_verts_range(vertsCount);
+    Graph_base_ (int Count = 0) : m_Verts() {
+        assert(Count > 0 &&
+               Count <= std::numeric_limits<int>::max() &&
+               "Invalid Value: Count");
+        Construct_verts(Count);
     }
 
-    void resize (size_type newCount) noexcept {
-        assert(newCount > 0 && newCount <= UINT_MAX && "Invalid Value: newCount");
-        if (newCount > m_Verts.size()) {
-            Construct_verts_range(newCount - m_Verts.size());
-        } else { // [NOTE]: it's expensive and never should be used!
-            while (m_Verts.size() > newCount) {
-                m_Verts.erase(m_Verts.end());
-            }
+    void add_n_verts (int Count) noexcept {
+        assert(Count > 0 &&
+               Count <= std::numeric_limits<int>::max() - this->size() &&
+               "Invalid Value: Count");
+        int startNo = 1 + m_Verts.size();
+        Construct_verts(Count, startNo);
+    }
+
+    Graph_base_& operator= (const Graph_base_&) = delete;
+    Graph_base_ (const Graph_base_&)            = delete;
+
+    ~Graph_base_() { 
+        // [NOTE]: does it really work? (= yes, valgrind approves)
+        // [TODO]: better way?
+        for (auto Vit = m_Verts.begin(); Vit != m_Verts.end(); ++Vit) {
+            delete *Vit;
         }
     }
 
-    // Graph_base_& operator= (const Graph_base_&) = delete;
-    // Graph_base_ (const Graph_base_&)            = delete;
-
-    size_type size() const noexcept { return m_Verts.size(); }
+    int size() const noexcept { return (int)m_Verts.size(); }
 
 private:
-    void Construct_verts_range (size_type vertsCount) noexcept {
-        for (vert vi {1}; vi <= vertsCount; ++vi) {
-            m_Verts.insert(vi);
+    void Construct_verts (int Count, int startNo = 1) noexcept { // [TODO]: is there a better way?
+        for (int i {0}; i < Count; ++i) {
+            vert* Vnew = new vert(startNo + i);
+            m_Verts.insert(Vnew);
         }
     }
 
 public:
-    mutable std::set<vert> m_Verts;
+    mutable std::set<vert*> m_Verts;
 };
 
 
@@ -64,34 +79,49 @@ struct weighted_graph : Graph_base_ {
     using size_type   = Mybase::size_type;
     using vert        = Mybase::vert;
 
-    // generate graph with vertsCount*vertsCount adjacency matrix filled 0
-    weighted_graph(size_type vertsCount) : Graph_base_(vertsCount) {
-        m_Adjmatrix = std::vector<std::vector<int>>(vertsCount, std::vector<int>(vertsCount, 0));
+    // generate graph with Count verts and zero weight function
+    weighted_graph (size_type Count) : Graph_base_(Count) {
+        m_Weightfunc = std::vector<std::vector<int>>(Count, std::vector<int>(Count, 0));
     }
 
+    // generate graph with weight function from file
     weighted_graph (std::string Filename) : Graph_base_() {
-        m_Adjmatrix = generate_vector_from_file(Filename);
-        Mybase::resize(m_Adjmatrix.size());
+        m_Weightfunc = generate_2dvector_from_file(Filename);
+        Mybase::add_n_verts(m_Weightfunc.size());
+        Construct_edges();
     }
 
     weighted_graph& operator= (const weighted_graph& Rhs) = default;
 
-    virtual std::map<weight_type, edge> get_edges() const noexcept {
-        std::map<weight_type, edge> Result;
-        size_type sz = Mybase::size();
-        for (vert vi {0}; vi < sz; ++vi) {
-            for (vert vj {vi + 1}; vj < sz; ++vj) {
-                weight_type weight = m_Adjmatrix[vi][vj];
+    // std::map<weight_type, edge> get_edges() const noexcept {
+    //     return m_Edges;
+    // }
+
+private:
+    // construct edges by weight function
+    void Construct_edges() noexcept {
+        for (vert vi {0}; vi < size(); ++vi) {
+            for (vert vj {vi + 1}; vj < size(); ++vj) {
+                weight_type weight = m_Weightfunc[vi][vj];
                 if (weight) {
-                    Result.insert({ weight, {vi,vj} });
-                    Result.insert({ weight, {vj,vi} });
+                    m_Edges.emplace(weight, {vi + 1, vj + 1});
                 }
             }
+
         }
     }
 
-
-    std::vector<std::vector<Weight_t>> m_Adjmatrix; // graph presented by Adjacency Matrix
+public:
+    // weighted_graph must have a weight function on it's
+    // edges; weight of edge e is equals
+    // m_Weightfunc[e.first][e.secont]
+    std::vector<std::vector<Weight_t>> m_Weightfunc;
+    mutable std::multimap<Weight_t, edge> m_Edges;
+    // [TODO]: key = 0 is unacceptable
+    // [TODO]: try make edge as std::pair<*vert, *vert>;
+    //         1. to connect m_Verts and m_Edges
+    //         2. replace one vert to another (edge contraction)
+    //            is nice with pointers
 };
 
 
